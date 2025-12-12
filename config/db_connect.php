4243d57bd725eab87cb3990a2bf63fc60480f895
white22266 <peckjianhao0226@gmail.com>
2025-12-13 01:06:39 +0800

    Add db_connect.php

---
+++ config/db_connect.php
@@ -0,0 +1,12 @@
+<?php
+// config/db_connect.php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "travel_itinerary_db";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
